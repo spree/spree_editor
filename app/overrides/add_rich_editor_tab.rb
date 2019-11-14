224ce4fcf394633e348fe44f5081490e6d42c120
z4ppy <z4ppy.bbc@gmail.com>
2019-11-14 11:28:38 +0100

    Fix admin menu link layout

--- app/overrides/add_rich_editor_tab.rb
+++ app/overrides/add_rich_editor_tab.rb
@@ -1,6 +1,7 @@
+
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
   name: 'add_rich_editor_tab',
   insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text: '<li<%== " class=\"active\"" if controller.controller_name == "editor_settings" %>><%= link_to Spree.t(:rich_editor), spree.edit_admin_editor_settings_path %></li>'
+  text: "<%= tab Spree::Page, url: spree.edit_admin_editor_settings_path, label: Spree.t(:rich_editor) %>"
 )
